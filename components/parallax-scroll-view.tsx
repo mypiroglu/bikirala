d2c5608bc28bbe31d6af3b695fbb2ff9fa6f08ee
mypiroglu <46695094+mypiroglu@users.noreply.github.com>
2025-10-23 00:14:43 +0300

    refactor: extract styles into dedicated files

--- components/parallax-scroll-view.tsx
+++ components/parallax-scroll-view.tsx
@@ -1,5 +1,4 @@
 import type { PropsWithChildren, ReactElement } from 'react';
-import { StyleSheet } from 'react-native';
 import Animated, {
   interpolate,
   useAnimatedRef,
@@ -10,8 +9,7 @@ import Animated, {
 import { ThemedView } from '@/components/themed-view';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { useThemeColor } from '@/hooks/use-theme-color';
-
-const HEADER_HEIGHT = 250;
+import { HEADER_HEIGHT, styles } from './parallax-scroll-view.styles';
 
 type Props = PropsWithChildren<{
   headerImage: ReactElement;
@@ -62,18 +60,3 @@ export default function ParallaxScrollView({
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  header: {
-    height: HEADER_HEIGHT,
-    overflow: 'hidden',
-  },
-  content: {
-    flex: 1,
-    padding: 32,
-    gap: 16,
-    overflow: 'hidden',
-  },
-});
